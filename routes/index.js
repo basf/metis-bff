c348d9bdb6d95262634179c4cd25ab53631a1ee4
Pavel Malyshev <pmalyshev@devexperts.com>
2021-10-15 22:21:57 +0300

    add heartbeat support

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 module.exports = {
+    head,
     get,
 };
 
 async function get(req, res) {
     res.json({ path: 'index' });
+}
+
+async function head(req, res) {
+    res.sse.send('pong');
+    return res.status(204).end();
 }
