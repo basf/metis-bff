3903290da6a3a14800da8a29f1a38ed608514be8
Valexr <valexr@gmail.com>
2022-02-25 14:06:06 +0300

    fix deprecated `substr`

--- index.js
+++ index.js
@@ -19,54 +19,54 @@ const app = express();
 secure && app.set('trust proxy', 1); // if nginx used
 
 bff(app, {
-    security: {
-        cors: {
-            credentials: true,
-            origin: true,
-        },
-        csrf: false,
-        secure,
-    },
-    session: {
-        persist: true,
-        cookie: {
-            secure: false, // TODO FIXME?
-            httpOnly: true,
-            sameSite: true,
-            maxAge: 86400000,
-        },
-    },
-    sse: {
-        path: '/stream',
-    },
-    api: {
-        dir: path.join(__dirname, 'routes'),
-    },
+	security: {
+		cors: {
+			credentials: true,
+			origin: true,
+		},
+		csrf: false,
+		secure,
+	},
+	session: {
+		persist: true,
+		cookie: {
+			secure: false, // TODO FIXME?
+			httpOnly: true,
+			sameSite: true,
+			maxAge: 86400000,
+		},
+	},
+	sse: {
+		path: '/stream',
+	},
+	api: {
+		dir: path.join(__dirname, 'routes'),
+	},
 	proxy: {
 		target: backend.baseURL,
 	},
-    static: false,
-    ssr: false,
-    middlewares: [
-        passport.initialize(),
-        passport.session(),
-        sseMiddleware,
-    ]
+	static: false,
+	ssr: false,
+	middlewares: [
+		passport.initialize(),
+		passport.session(),
+		sseMiddleware,
+	]
 });
 
 app.use((err, req, res, next) => {
-    const status = err.status || 400;
-    const error = err || { status, error: getReasonPhrase(status) };
+	const status = err.status || 400;
+	const error = err || { status, error: getReasonPhrase(status) };
 
-    console.error(JSON.stringify(error).substr(0, 500) + '...');
+	console.error(JSON.stringify(error).substring(0, 500) + '...');
 
-    if (res.headersSent) {
-        res.sse.sendTo([ error ], 'errors');
-    } else {
-        res.status(status).json(error);
-    }
+	if (res.headersSent) {
+		res.sse.sendTo([error], 'errors');
+	} else {
+		res.status(status).json(error);
+	}
 });
 
 app.listen(PORT, () => {
-    console.log(`App in dev-mode=${dev} listens to http://localhost:${PORT}`);
+	console.log(`App in dev-mode=${dev} listens to http://localhost:${PORT}`);
 });
