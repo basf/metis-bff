2e691761f25911805f01ba21d891abafc5699c49
Valexr <valexr@gmail.com>
2022-04-29 20:26:10 +0300

    /datasources/:id/collections

---
+++ routes/[version]/datasources/[id]/collections.js
@@ -0,0 +1,24 @@
+const { StatusCodes } = require('http-status-codes');
+const { checkAuth } = require('../../../../middlewares/auth');
+const { getUserCollections } = require('../../../../middlewares/db');
+const { delsertDataSourceCollections } = require('../../../../services/db');
+
+module.exports = {
+    patch: [
+        checkAuth,
+        patch,
+        getUserCollections,
+        (req, res) => res.sse.sendTo({ reqId: req.id, data: req.session.collections }, 'collections')
+    ],
+};
+
+async function patch(req, res, next) {
+    const reqId = req.id;
+    res.status(StatusCodes.ACCEPTED).json({ reqId });
+    try {
+        await delsertDataSourceCollections(req.params.id, req.body);
+        return next();
+    } catch (error) {
+        return next({ error });
+    }
+}
