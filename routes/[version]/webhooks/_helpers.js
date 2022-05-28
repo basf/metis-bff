e997eba8bc7998ced9310964e81963bd2e0b6ad3
Valexr <valexr@gmail.com>
2022-05-28 09:47:13 +0300

    prepare result as datasources for SSE

--- routes/[version]/webhooks/_helpers.js
+++ routes/[version]/webhooks/_helpers.js
@@ -11,6 +11,7 @@ module.exports = { getAndPrepareCalculationsWithResult };
 
 async function getAndPrepareCalculationsWithResult(userId, calculations, result) {
     const output = await getAndPrepareCalculations(calculations);
+    let dataSources = [];
     // result processing
     for (const data of result) {
         const { parentUUID, uuid } = data;
@@ -18,8 +19,10 @@ async function getAndPrepareCalculationsWithResult(userId, calculations, result)
         const parentCollections = await selectUserCollectionsByDataSources(userId, [parentDataSourceId]);
         const dataSource = await insertUserDataSource(userId, { uuid });
         const dataSourceCollections = await delsertDataSourceCollections(dataSource.id, parentCollections);
+        dataSources.push(dataSource);
     }
     // add result to calculations SSE output
     const calcId = calculations.find(c => c.uuid === uuid).id;
+    result = await getAndPrepareDataSources(dataSources);
     return output.map(calc => calc.id === calcId ? { ...calc, result } : calc);
 }
