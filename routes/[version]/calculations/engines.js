861fe2146838e63e1a0d6025170b0fb77f9850c6
Valexr <valexr@gmail.com>
2022-07-29 19:55:27 +0300

    engines

---
+++ routes/[version]/calculations/engines.js
@@ -0,0 +1,14 @@
+const { getEngines } = require('../../../services/backend');
+
+module.exports = {
+    get,
+};
+
+async function get(req, res, next) {
+    try {
+        const output = await getEngines();
+        res.json(output);
+    } catch (error) {
+        return next({ error });
+    }
+}
