cfde2a656e9c9c39c57e599aabd6f2e5bd0894e7
Evgeny Blokhin <eb@tilde.pro>
2022-10-11 14:46:32 +0000

    Upgrade auth provider

--- routes/[version]/auth/basf.js
+++ routes/[version]/auth/basf.js
@@ -1,34 +1,46 @@
 
+const https = require('https');
+const querystring = require('querystring');
 const util = require('util');
 const OAuth2Strategy = require('passport-oauth2').Strategy;
 
 function Strategy(options = {}, verify) {
     OAuth2Strategy.call(this, options, verify);
     this.name = 'basf';
-    this._userProfileURL = options.userProfileURL;
 }
 
 util.inherits(Strategy, OAuth2Strategy);
 
 Strategy.prototype.userProfile = async function (accessToken, done) {
-    this._oauth2.get(this._userProfileURL, accessToken, function (err, body, res) {
-        if (err) {
-            if (err.data) {
-                try {
-                    json = JSON.parse(err.data);
-                } catch (_) { }
-            }
-            if (json && json.message) {
-                return done(new APIError(json.message));
-            }
-            return done(new OAuth2Strategy.InternalOAuthError('Failed to fetch user profile', err));
-        }
-        try {
-            return done(null, JSON.parse(body));
-        } catch (ex) {
-            return done(new Error('Failed to parse user profile'));
-        }
-    });
+	const post_data = querystring.stringify({}); // FIXME post data are void
+	const req = https.request({
+		host: 'graph.microsoft.com', // FIXME move to settings
+		port: 443,
+		path: '/oidc/userinfo',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(post_data),
+			'Authorization': 'Bearer ' + accessToken
+		}
+	}, function(resp){
+		let result = '';
+		resp.on('data', function(chunk){
+			result += chunk;
+		});
+		resp.on('end', function(){
+			try {
+				return done(null, JSON.parse(result));
+			} catch (err){
+				return done(new OAuth2Strategy.InternalOAuthError('Failed to parse user profile', err));
+			}
+		});
+	}).on('error', function(err){
+		return done(new OAuth2Strategy.InternalOAuthError('Failed to fetch user profile', err));
+	});
+	req.write(post_data);
+	req.end();
 };
 
 module.exports = Strategy;
+
