8baabf9072315ae5a71a6ea919e7f8f2b12acefd
Evgeny Blokhin <eb@tilde.pro>
2022-10-14 01:23:07 +0200

    Refactor tag adding script

--- scripts/create_collection.py
+++ scripts/create_collection.py
@@ -7,7 +7,7 @@ from config import config, TABLES, COLLECTION_VISIBILITY
 
 
 target = {}
-for n, item in enumerate(['uid', 'tid', 'title', 'descr'], 1):
+for n, item in enumerate(['user_id', 'type_id', 'title', 'descr'], 1):
     try: target[item] = sys.argv[n]
     except IndexError: raise RuntimeError("Required %s" % item)
 
@@ -21,15 +21,24 @@ conn = pg8000.connect(
 cursor = conn.cursor()
 
 
-def create_tag(conn, cursor, uid, tid, title, descr, visibility=COLLECTION_VISIBILITY['private']):
+def create_tag(conn, cursor, user_id, type_id, title, descr, visibility=COLLECTION_VISIBILITY['community']):
     cursor.execute("""
-    INSERT INTO {tags} ("userId", "typeId", title, description, visibility) VALUES ({uid}, {tid}, '{title}', '{descr}', '{visibility}') RETURNING id;
-    """.format(tags=TABLES['collections'], uid=uid, tid=tid, title=title, descr=descr, visibility=visibility))
+    INSERT INTO {tags} ("userId", "typeId", title, description, visibility) VALUES
+    ({user_id}, {type_id}, '{title}', '{descr}', '{visibility}')
+    RETURNING id;
+    """.format(
+        tags=TABLES['collections'],
+        user_id=user_id,
+        type_id=type_id,
+        title=title,
+        descr=descr,
+        visibility=visibility
+    ))
     conn.commit()
     return cursor.fetchone()[0]
 
 
-tag_id = create_tag(conn, cursor, target['uid'], target['tid'], target['title'], target['descr'])
+tag_id = create_tag(conn, cursor, target['user_id'], target['type_id'], target['title'], target['descr'])
 print(tag_id)
 
 conn.close()
