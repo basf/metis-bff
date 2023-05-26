158bdf18ecb4895e5348e86e454ce5e22f22a8d6
Askar Yusupov <devex.soft@gmail.com>
2023-05-26 12:12:37 +0500

    refactor: add a migration for users table

---
+++ migrations/20230526070609_users.js
@@ -0,0 +1,30 @@
+const { USERS_TABLE, NAME_LENGTH, PASSWORD_LENGTH, FOREIGN_KEY_LENGTH } = require('../services/db');
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable(USERS_TABLE, (table) => {
+        table.increments('id');
+        table.string('password', PASSWORD_LENGTH).nullable();
+        table.string('firstName', NAME_LENGTH);
+        table.string('lastName', NAME_LENGTH);
+        table.integer('roleId', FOREIGN_KEY_LENGTH).unsigned().index();
+        table.timestamps(false, true, true);
+
+        table
+            .foreign('roleId', 'fk_roleId')
+            .references('id')
+            .inTable(USER_ROLES_TABLE)
+            .onDelete('CASCADE');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable(USERS_TABLE);
+};
