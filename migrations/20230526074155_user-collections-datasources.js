4010b3f7cfa7684473dd22a22fb4ca596d13477f
Askar Yusupov <devex.soft@gmail.com>
2023-05-26 12:44:37 +0500

    feat(migrations): add user_collections_datasources table to database schema to support many-to-many relationship between user_collections and user_datasources tables

---
+++ migrations/20230526074155_user-collections-datasources.js
@@ -0,0 +1,35 @@
+const { USER_COLLECTIONS_DATASOURCES_TABLE, USER_COLLECTIONS_TABLE } = require('../services/db');
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable(USER_COLLECTIONS_DATASOURCES_TABLE, (table) => {
+        table.integer('collectionId').unsigned().index();
+        table.integer('dataSourceId').unsigned().index();
+        table.timestamps(false, true, true);
+
+        table.primary(['collectionId', 'dataSourceId'], {
+            constraintName: 'pk_collection_dataSource',
+        });
+        table
+            .foreign('collectionId', 'fk_collectionId')
+            .references('id')
+            .inTable(USER_COLLECTIONS_TABLE)
+            .onDelete('CASCADE');
+        table
+            .foreign('dataSourceId', 'fk_dataSourceId')
+            .references('id')
+            .inTable(USER_DATASOURCES_TABLE)
+            .onDelete('CASCADE');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable(USER_COLLECTIONS_DATASOURCES_TABLE);
+};
