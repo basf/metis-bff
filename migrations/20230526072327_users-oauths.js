22d60b83a1e288731d909ab2e1af781880436466
Askar Yusupov <devex.soft@gmail.com>
2023-05-26 12:26:34 +0500

    feat(migrations): add users-oauths table to database schema to support OAuth authentication
    This commit adds a new migration file that creates a new table called `users-oauths` in the database schema. This table is used to store user information obtained from OAuth providers. The table has columns for `userId`, `provider`, `providerId`, `profile`, and `timestamps`. The `userId` column is a foreign key that references the `id` column in the `users` table. The `provider` column is an enumeration that specifies the OAuth provider used for authentication. The `providerId` column is a unique string that identifies the user on the OAuth provider's platform. The `profile` column is a JSON object that contains additional user information obtained from the OAuth provider. This table is necessary to support OAuth authentication in the application.

---
+++ migrations/20230526072327_users-oauths.js
@@ -0,0 +1,40 @@
+const {
+    USERS_TABLE,
+    USERS_OAUTHS_TABLE,
+    OAUTH_PROVIDERS_ENUM,
+    FOREIGN_KEY_LENGTH,
+} = require('../services/db');
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable(USERS_OAUTHS_TABLE, (table) => {
+        table.integer('userId', FOREIGN_KEY_LENGTH).unsigned().index();
+        table.enu('provider', OAUTH_PROVIDERS_ENUM, {
+            useNative: true,
+            enumName: 'oauth_providers',
+        });
+        table.string('providerId').unique();
+        table.jsonb('profile').nullable();
+        table.timestamps(false, true, true);
+
+        table.primary(['userId', 'provider'], {
+            constraintName: 'pk_user_provider',
+        });
+        table
+            .foreign('userId', 'fk_userId')
+            .references('id')
+            .inTable(USERS_TABLE)
+            .onDelete('CASCADE');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable(USERS_OAUTHS_TABLE);
+};
