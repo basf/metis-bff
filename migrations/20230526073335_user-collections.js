41fd8a1452fcc007385bd727ace6075fdf8b0fe2
Askar Yusupov <devex.soft@gmail.com>
2023-05-26 12:37:05 +0500

    feat(migrations): add user_collections table to the database schema to store user collections
    This commit adds a new migration file that creates a new table called user_collections in the database schema. This table has columns for id, userId, typeId, title, description, visibility, and timestamps. The userId and typeId columns are foreign keys that reference the id column of the users and collections_types tables respectively. The visibility column is an enum that specifies the visibility of the collection. This table will be used to store user collections.

---
+++ migrations/20230526073335_user-collections.js
@@ -0,0 +1,47 @@
+const {
+    USERS_TABLE,
+    USER_COLLECTIONS_TABLE,
+    FOREIGN_KEY_LENGTH,
+    VISIBILITY_ENUM,
+    COLLECTIONS_TYPES_TABLE,
+} = require('../services/db');
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable(USER_COLLECTIONS_TABLE, (table) => {
+        table.increments('id');
+        table.integer('userId', FOREIGN_KEY_LENGTH).unsigned().index();
+        table.integer('typeId', FOREIGN_KEY_LENGTH).unsigned().index();
+        table.string('title', 32);
+        table.string('description', 64);
+        table
+            .enu('visibility', VISIBILITY_ENUM, {
+                useNative: true,
+                enumName: 'collection_visibility',
+            })
+            .defaultTo(VISIBILITY_ENUM[0]);
+        table.timestamps(false, true, true);
+
+        table
+            .foreign('userId', 'fk_userId')
+            .references('id')
+            .inTable(USERS_TABLE)
+            .onDelete('CASCADE');
+        table
+            .foreign('typeId', 'fk_typeId')
+            .references('id')
+            .inTable(COLLECTIONS_TYPES_TABLE)
+            .onDelete('CASCADE');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable(USER_COLLECTIONS_TABLE);
+};
