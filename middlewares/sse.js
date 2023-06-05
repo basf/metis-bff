06d38db279d1115fb0a0c14ae8a418eef8030973
Askar Yusupov <devex.soft@gmail.com>
2023-06-05 19:30:55 +0500

    refactor(sse.js): refactor filterUser function to createFilter and add support for filtering shared collections by user id.

--- middlewares/sse.js
+++ middlewares/sse.js
@@ -1,17 +1,51 @@
 const { hasAuthBearerHeader } = require('./apiToken');
 
 module.exports = function (req, res, next) {
-    function filterUser({ session, user }) {
-        const userId = session?.passport?.user || user?.id;
-        return req.user && req.user.id === userId;
+    function createFilter(data, event, _id) {
+        let newData = null;
+        function filterByUserId(userId) {
+            newData = { ...data };
+            return req.user && req.user.id === userId;
+        }
+
+        function filterSharedCollections(userId) {
+            newData = null;
+            if (event !== 'collections') {
+                return false;
+            }
+
+            const sharedCollections = data.data?.filter(({ visibility, users }) => {
+                return visibility === 'shared' && users?.includes(userId);
+            });
+
+            if (sharedCollections.length) {
+                newData = { ...data, data: sharedCollections, total: sharedCollections.length };
+                return true;
+            }
+            return false;
+        }
+
+        let userId;
+        return {
+            user({ session, user }) {
+                userId = session?.passport?.user || user?.id;
+                return filterByUserId(userId) || filterSharedCollections(userId);
+            },
+            data: {
+                toJSON() {
+                    return newData;
+                },
+            },
+        };
     }
 
-    res.sse.sendTo = function (...args) {
+    res.sse.sendTo = function (data, event, id) {
+        const filters = createFilter(data, event, id);
         if (hasAuthBearerHeader(req)) {
-            this.send(filterUser, ...args);
+            this.send(filters.user, filters.data, event, id);
         } else {
             req.session.save(() => {
-                this.send(filterUser, ...args);
+                this.send(filters.user, filters.data, event, id);
             });
         }
     };
